b452888d36b269c749b1f9270d365e2194ee4417
Bruna Xavier <brunabxsantos@gmail.com>
2013-11-29 01:33:05 -0200

    Adicionando novo critério para a avaliação do AG

--- ga_fitness_function.m
+++ ga_fitness_function.m
@@ -8,6 +8,7 @@ function result = ga_fitness_function(genome, training_set)
     
     % encontra os pesos das instancias de treinamento classificadas (ou nao) como a
     % regra
+    w = [training_set(:).w];
     w_matches = [training_set(training_set_class_rule_matches).w]';
     w_nomatches = [training_set(training_set_class_rule_nomatches).w]';
     
@@ -16,11 +17,19 @@ function result = ga_fitness_function(genome, training_set)
     mu_matches = rule.mu(training_set(training_set_class_rule_matches));
     mu_nomatches = rule.mu(training_set(training_set_class_rule_nomatches));
     
-    % numero de instâncias de treinamento cobertas pela regra Ri
-    % comparada com o numero de instâncias de treinamento que são da classe
+    % numero de instancias de treinamento cobertas pela regra Ri
+    % comparada com o numero de instancias de treinamento que sao da classe
     % ci
     n = sum(w_matches .* mu_matches) / sum(w_matches);
     f1 = n;
     
-    result = f1;
+    % fracao das instancias cobertas pela regra Ri independente da classe
+    % a regra deve ser geral o suficiente de maneira que cubra uma fracao
+    % significante (kcov) de todas as instancias ao inves de representar
+    % outliers no conjunto de treinamento
+    n = sum(w_matches .* mu_matches) / sum(w);
+    kcov = 0.8;
+    f2 = 1 * (n > kcov) + (n/kcov) * (n <= kcov);
+    
+    result = f1 * f2;
 end
