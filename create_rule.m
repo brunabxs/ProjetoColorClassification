c7d8ad9ce05d57fa8cc2da4fb9498e4d3fd31696
Bruna Xavier <brunabxsantos@gmail.com>
2013-11-29 00:50:51 -0200

    [REFACTOR] Criação da regra a partir do cromossomo
    - A função de inclusão ao invés de receber uma instância, recebe um
    conjunto

--- create_rule.m
+++ create_rule.m
@@ -1,32 +1,39 @@
 function rule = create_rule(genome, training_set)
     A = genome(1:12);
     S = genome(13:15);
-    rule = struct('A', A, 'S', S, 'mf', get_parameters(A), 'mu', @(instance) mu(A, S, instance), 'class', @() rule_class(A, S, training_set));
+    rule = struct('A', A, 'S', S, 'mf', get_parameters(A), 'mu', @(set) mu(A, S, set), 'class', rule_class(A, S, training_set));
 end
 
 function result = rule_class(A, S, training_set)
     num_classes = max([training_set(:).class]);
     classes = zeros(1, num_classes);
 
-    for i = 1 : numel(training_set);
-        training_instance = training_set(i);
-        classes(training_instance.class) = classes(training_instance.class) + mu(A, S, training_instance);
+    % encontra os graus de pertinencia de cada instancia de treinamento
+    mu_training_set = mu(A, S, training_set);
+    
+    % para cada classe, calcula a soma dos graus de pertinencia das
+    % instancias desta classe
+    for i = 1 : numel(classes);
+        positions = find([training_set(:).class] == i);
+        mu_training_set(positions);
+        classes(i) = sum(mu_training_set(positions));
     end
     
-    [~, max_pos] = max(abs(classes));
+    % encontra a classe que possui a maior soma
+    [~, max_pos] = max(classes);
     result = max_pos;
 end
 
-function result = mu(A, S, instance)
+function result = mu(A, S, set)
     A1 = A(1:4);
     A2 = A(5:8);
     A3 = A(9:12);
     
-    muA1 = evalmmf(instance.r, get_parameters(A1), 'trapmf');
-    muA2 = evalmmf(instance.g, get_parameters(A2), 'trapmf');
-    muA3 = evalmmf(instance.b, get_parameters(A3), 'trapmf');
-    
-    result = min([muA1, muA2, muA3]);
+    result = arrayfun(@(instance) min([eval(instance.r, A1), eval(instance.g, A2), eval(instance.b, A3)]), set);
+end
+
+function result = eval(attribute, A)
+    result = trapmf(attribute, get_parameters(A));
 end
 
 function result = get_parameters(A)
